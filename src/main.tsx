b2b0a9f30bf04e3c6331dd7cb0e2462df50c9d49
Trueman Phamdo <trueman@phamdo.com>
2024-11-12 10:37:14 -0500

    Add meta tags

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import About from './about.tsx'
-import Work from './work.tsx'
-import Project from './project.tsx'
-import {
-  createHashRouter,
-  RouterProvider,
-  Navigate,
-} from "react-router-dom";
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import About from './about.tsx';
+import Work from './work.tsx';
+import Project from './project.tsx';
+import { createHashRouter, RouterProvider, Navigate } from 'react-router-dom';
+import './index.css';
 
 const router = createHashRouter([
   {
-    path: "/",
-    element: <Work />
+    path: '/',
+    element: <Work />,
   },
   {
-    path: "/about",
-    element: <About />
+    path: '/about',
+    element: <About />,
   },
   {
-    path: "/work/:projectName",
-    element: <Project />
+    path: '/work/:projectName',
+    element: <Project />,
   },
   {
-    path: "*",
-    element: <Navigate to="/" replace />
+    path: '*',
+    element: <Navigate to="/" replace />,
   },
 ]);
 
@@ -33,4 +29,4 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+);
